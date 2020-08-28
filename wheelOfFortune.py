8d1b7393b689fd06d224201bf5ce090e659a77d3
KJTempas <fw6927mm@go.minneapolis.edu>
2020-08-28 11:04:58 -0500

    work on updating code phrase, added dollars,vowel

--- wheelOfFortune.py
+++ wheelOfFortune.py
@@ -3,63 +3,97 @@ import re, random
 #todo - random phrase
 
 def main():
-    phrase = select_phrase() #call method s_p and return phrase
-    coded_phrase = code_phrase(phrase) #call method 
+    phrase = select_phrase() #call method and return phrase
+    coded_phrase = code_phrase(phrase) 
+    print('Let\'s play Wheel of Fortune! You will be given a coded phrase. Spin and select a consonant.')
+    print('Consonant in phrase means you keep the money.')
+    print('Vowels can be purchased for 100 each.')
+    print()
     print(f'Here is your mystery phrase: {coded_phrase}')
-    user_guess(phrase, coded_phrase)
+    dollar = spin_wheel() #call method and return dollar value
+    user_guess(phrase, coded_phrase, dollar)
 
 
 def select_phrase():
-    phrases = ["No news is good news", "An apple a day keeps the doctor away", "A friend in need is a friend indeed"]
+    phrases = ["No news is good news"]#, "An apple a day keeps the doctor away", 
+   # "A friend in need is a friend indeed"] #add these back after rest running
     # randomly select from array of phrases
     phrase = random.choice(phrases) #info from https://pynative.com/python-random-choice/-select random from a list
     print(phrase)
     #example phrase for initial testing
     #phrase = "No news is good news"
     phrase = phrase.upper()  #make phrase upper for simpler comparison later (and since Wheel of Fortune uses all caps)
-    #print(phrase)
     return phrase
 
 def code_phrase(phrase):
-    # show user blanks so they know how many letters and spacing
-    #code phrase - let * represent letters
+    # show user blanks so they know how many letters and spacing- let * represent letters
     coded_phrase = re.sub('[A-Za-z]', '*', phrase)
     #print(coded_phrase) 
     return coded_phrase
 
-def user_guess(phrase, coded_phrase):
-    letter =input('Guess a letter that you think is in the phrase ->  ')
+def spin_wheel():
+    dollars = [0,500,600,700,800,900]
+    dollar = random.choice(dollars)
+    print(dollar)
+    #user_guess(phrase, code_phrase)
+    return dollar
+
+def user_guess(phrase, coded_phrase,dollar):
+    total = 0
+    used_consonants = []
+    letter =input('Guess a consonant that you think is in the phrase ->  ')
     #some validation here - is it a letter? isalpha
     #think about upper/lower case -make it all upper
     letter = letter.upper()
+    #TODO see if letter already in used_consonants - alert if already guessed that letter
+    if letter in used_consonants:
+        print('You have already selected that consonant. Select another. ')
+        user_guess(phrase, code_phrase, dollar)
     if letter in phrase:
         print('Yes - that letter is in the phrase!')
+        total+= dollar  #add $ to contestant's bank
         #modify coded phrase to add letter
+        used_consonants.append(letter)
+        print('uc', used_consonants)
         update_coded_phrase(letter, phrase, coded_phrase) #call method below sending it 3 parameters
-        guess_phrase(phrase)
+        guess_phrase(phrase) 
     else:
         print('Sorry - that letter is not in the phrase')
+        buyOrSpin(dollar)
+        
+def buyOrSpin(dollar):
+    buyOrSpin = input('Do you want to purchase a vowel or spin for a consonant?  Enter V or C')
+    if buyOrSpin.upper() == 'V':
+        buyVowel(dollar)
+    elif buyOrSpin.upper() == 'C':
+        spin_wheel()
+    else:
+        print('Please enter either V or C')
+        buyOrSpin(dollar)
+
+
+def buyVowel(dollar):
+    print(f'Your current balance is {dollar}')
+    vowels = ["A", "E", "I", "O", "U"]
+
 
 def update_coded_phrase(letter ,phrase, code_phrase):
     print(letter) #prints N
     #x=re.findall(letter, phrase)
     iterator= re.finditer(letter, phrase) #finditer -Find all substrings where the RE matches, and returns them 
     #as an iterator. from https://docs.python.org/3/howto/regex.html
-    
-    for match in iterator:
-        print(match.span()) #span lists the start and end index of the match
-        print(match.span([0][0]))  #need to get index of each match; getting tuple
-        #re.sub()
-    
-    #print(x) #shows aray of 3 N's
-    #NEED TO WORK ON THIS AREA - need to knowindex of these 3 Ns
+    #https://stackoverflow.com/questions/2674391/python-locating-the-position-of-a-regex-match-in-a-string/16360404
+    indices = [m.start(0) for m in iterator]
+    print(indices)#works - gives [array of ints]
     
     #replace the * at those indices (use regex) with the letter
-    #sub this with this in this - wont' know where  -need index
-      #  re.sub('*',letter, code_phrase)  
-       # print(code_phrase)
-    #return the recoded phrase
-    #pass
+    for i in indices:  #try replace
+      #  re.sub('*',"letter", code_phrase[i])  
+        code_phrase[i].replace('*', 'letter')
+    print(code_phrase)
+    print(f'The mystery phrase now looks like this {code_phrase}')
+    return code_phrase
+    
 def guess_phrase(phrase):
     want_to_guess_phrase = input('Would you like to guess the phrase? Enter Y or N')
     if want_to_guess_phrase.upper() == 'Y':
